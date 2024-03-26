dbe42580567fd18042d602be7fdc467f9455c768
Maxim Shilo <maximshilo.dev@gmail.com>
2024-03-26 17:50:02 +0200

    About me
    finished inputing the content of the about me page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -11,15 +11,101 @@ export default function About(props) {
         ['Languages', 'JavaScript', 'C#', 'Python'],
     ]
 
+    const education = [
+        ['B.Sc in Computer Science', '2024 - expected 2026', 'Bar Ilan University, Ramat Gan, Israel.'],
+        ['Full stack Development Certification', '2023 - 2024', 'SVCollege, Tel Aviv, Israel.'],
+        ['Highschool Diploma', '2014 - 2018', 'With coursework in Computer Science, Mathematics, and Physics.']
+    ]
+
+    const biography = String.raw`Hello, I'm Maxim Shilo, a fervent full stack developer based in the vibrant Tel Aviv district of Israel. At 25 years old, I stand at the intersection of technology and creativity, dedicated to weaving the art and science of computer science into every project I undertake.
+
+    My journey into the world of technology began in childhood, sparked by a simple yet transformative gift from my grandmother—a family PC. This early encounter with computing opened a gateway to a world where logic meets imagination, leading me down a path of endless fascination with technology and, specifically, computer science.
+    
+    What excites me most about full stack development is the holistic approach it offers—allowing me to craft complete, functional digital experiences from the ground up. It's not just about writing code; it's about sculpting digital environments that are as beautiful as they are functional, where every line of code and pixel of design resonates with the user's needs and aspirations.
+    
+    My passion for technology is matched only by my natural affinity for coding and algorithms. I thrive on the challenge of solving complex problems and the joy of bringing innovative ideas to life through meticulous coding and creative design. This blend of artistic expression and intellectual rigor is what makes full stack development not just my career, but my calling.
+    
+    I invite you to explore my portfolio, where each project showcases my dedication to crafting seamless, engaging digital experiences. Let's connect and create something extraordinary together.`
     const displaySkills = () => {
         return (
-                <div className='skills'>
-                    {
-                        skills.map((arr, i) => {
-                            
-                        })
-                    }
+            <div className='skills'>
+                {
+                    skills.map((arr) => {
+                        return (
+                            <div className='col'>
+                                <div className='sectionTitle'>{arr[0]}</div>
+                                <div className='line'></div>
+                                <ul>
+                                    {
+                                        arr.map((el, id) => {
+                                            if (id != 0) {
+                                                return (
+                                                    <li>{el}</li>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    const displayEducation = () => {
+        return (
+            <div className='skills'>
+                {
+                    education.map((arr) => {
+                        return (
+                            <div className='col'>
+                                <div className='sectionTitle'>{arr[0]}</div>
+                                <div className='line'></div>
+                                <ul>
+                                    {
+                                        arr.map((el, id) => {
+                                            if (id != 0) {
+                                                return (
+                                                    <li>{el}</li>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    const displayBiography = () => {
+        return (
+            <div className='skills'>
+                <div className='col'>
+                    <div className='sectionTitle'>
+                        About me
+                    </div>
+                    <p>
+                        Hello, I'm Maxim Shilo, a fervent full stack developer based in the vibrant Tel Aviv district of Israel. At 25 years old, I stand at the intersection of technology and creativity, dedicated to weaving the art and science of computer science into every project I undertake.
+                    </p>
+                    <p>
+                        My journey into the world of technology began in childhood, sparked by a simple yet transformative gift from my grandmother—a family PC. This early encounter with computing opened a gateway to a world where logic meets imagination, leading me down a path of endless fascination with technology and, specifically, computer science.
+                    </p>
+                    <p>
+                        What excites me most about full stack development is the holistic approach it offers—allowing me to craft complete, functional digital experiences from the ground up. It's not just about writing code; it's about sculpting digital environments that are as beautiful as they are functional, where every line of code and pixel of design resonates with the user's needs and aspirations.
+                    </p>
+                    <p>
+                        My passion for technology is matched only by my natural affinity for coding and algorithms. I thrive on the challenge of solving complex problems and the joy of bringing innovative ideas to life through meticulous coding and creative design. This blend of artistic expression and intellectual rigor is what makes full stack development not just my career, but my calling.
+                    </p>
+                    <p>
+                        I invite you to explore my portfolio, where each project showcases my dedication to crafting seamless, engaging digital experiences. Let's connect and create something extraordinary together.
+                    </p>
                 </div>
+            </div>
         )
     }
 
@@ -54,9 +140,17 @@ export default function About(props) {
                     </div>
 
                     <div className='row'>
+                        {displayEducation()}
+                    </div>
+
+                    <div className='row'>
                         <span className='mainHeading'>
                             Biography
                         </span>
+                    </div>
+
+                    <div className='row'>
+                        {displayBiography()}
                     </div>
                 </div>
             </div>
