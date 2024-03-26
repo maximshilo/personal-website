42b91c74ad72972343804dfbd6df48bcef5af2ac
Maxim Shilo <maximshilo.dev@gmail.com>
2024-03-26 15:49:12 +0200

    UPDATED RESUME

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 import Navbar from './Navbar.jsx'
 
-export default function About (props) {
+export default function About(props) {
+    const skills = [
+        ['Front-end', 'React', 'JavaScript', 'HTML5', 'CSS3'],
+        ['Back-end', 'Node.js', 'Express'],
+        ['Databases', 'MongoDB'],
+        ['Tools', 'Git', 'Heroku'],
+        ['Languages', 'JavaScript', 'C#', 'Python'],
+    ]
+
+    const displaySkills = () => {
+        return (
+                <div className='skills'>
+                </div>
+        )
+    }
+
     return (
         <div className='About'>
             <div className='con'>
@@ -19,7 +34,23 @@ export default function About (props) {
 
                     <div className='row'>
                         <span className='mainHeading'>
-                            Full Stack developer
+                            Technical Skills
+                        </span>
+                    </div>
+
+                    <div className='row'>
+                        {displaySkills()}
+                    </div>
+
+                    <div className='row'>
+                        <span className='mainHeading'>
+                            Education
+                        </span>
+                    </div>
+
+                    <div className='row'>
+                        <span className='mainHeading'>
+                            Biography
                         </span>
                     </div>
                 </div>
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Navbar from './Navbar.jsx'
 
-export default function Portfolio (props) {
+export default function Portfolio(props) {
     return (
         <div className='Portfolio'>
             <div className='con'>
@@ -18,9 +18,41 @@ export default function Portfolio (props) {
                     </div>
 
                     <div className='row'>
-                        <span className='mainHeading'>
-                            Full Stack developer
-                        </span>
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>ECHO</span></div>
+                            <div className='row'><span className='projectBody'>Social media application.</span></div>
+                            <div className='row'><span className='projectHeading'>UNDER CONSTRUCTION</span></div>
+                            <img className='projectImg' src='images/construction.png'></img>
+                        </div>
+
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>BASKET</span></div>
+                            <div className='row'><span className='projectBody'>E-commerce platform.</span></div>
+                            <div className='row'><span className='projectHeading'>UNDER CONSTRUCTION</span></div>
+                            <img className='projectImg' src='images/construction.png'></img>
+                        </div>
+
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>FINTRACK</span></div>
+                            <div className='row'><span className='projectBody'>Personal finance tracker.</span></div>
+                            <div className='row'><span className='projectHeading'>UNDER CONSTRUCTION</span></div>
+                            <img className='projectImg' src='images/construction.png'></img>
+                        </div>
+
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>DONIT</span></div>
+                            <div className='row'><span className='projectBody'>Project management tool.</span></div>
+                            <div className='row'><span className='projectHeading'>UNDER CONSTRUCTION</span></div>
+                            <img className='projectImg' src='images/construction.png'></img>
+                        </div>
+
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>SUDOKU</span></div>
+                            <div className='row'><span className='projectBody'>A puzzle game.</span></div>
+                            <div className='row'><span className='projectHeading'>UNDER CONSTRUCTION</span></div>
+                            <img className='projectImg' src='images/construction.png'></img>
+                        </div>
+
                     </div>
                 </div>
             </div>
