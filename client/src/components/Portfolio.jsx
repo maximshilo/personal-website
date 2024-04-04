6a31ae7e135fc7dbbadc8b412d56ad7db1c2cf80
Maxim Shilo <maximshilo.dev@gmail.com>
2024-04-04 10:04:59 +0300

    Added echo to the portfolio

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { useNavigate, Link } from 'react-router-dom'
 import Navbar from './Navbar.jsx'
 
 export default function Portfolio(props) {
+    const navigate = useNavigate()
     return (
         <div className='Portfolio'>
             <div className='con'>
@@ -19,9 +20,23 @@ export default function Portfolio(props) {
 
                     <div className='row'>
                         <div className='projectCon'>
-                            <div className='row'><span className='projectHeading'>ECHO</span></div>
-                            <div className='row'><span className='projectBody'>Social media application.</span></div>
-                            <img className='projectImg' src='images/construction.png'></img>
+                                <div className='row'><span className='projectHeading'>ECHO</span></div>
+                                <div className='row'><span className='projectBody'>Social media application.</span></div>
+                                <div className='row'>
+                                    <Link target="_blank" to={'https://echo-application-d49a62c92c1a.herokuapp.com/'} className='projectBody'>Live version</Link>
+                                    <div className='empty'></div>
+                                    <Link target="_blank" to={'https://github.com/maximshilo/echo.git'} className='projectBody'>GitHub repository</Link>
+                                </div>
+                                <img className='projectImg' src='images/echo.png'></img>
+                                <div className='row'>
+                                    <p>
+                                    Echo is a Twitter-inspired application developed using React, MongoDB, Express, and Node.js (MERN stack). 
+                                    With Echo, users can post short messages, follow other users, and engage with content in real-time. 
+                                    Echo provides a seamless and intuitive user experience for sharing thoughts and connecting with others. 
+                                    Whether it's expressing opinions, sharing updates, or fostering discussions, 
+                                    Echo offers a familiar platform for social interaction and networking.
+                                    </p>
+                                </div>
                         </div>
 
                         <div className='projectCon'>
