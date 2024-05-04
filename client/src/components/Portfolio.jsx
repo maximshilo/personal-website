c9b53b0c4e3fb00e2706f51f6e5ed8eae4b014e2
Maxim Shilo <maximshilo.dev@gmail.com>
2024-05-04 18:50:57 +0300

    Added the Sudoku project
    added content to Portfolio.jsx
    added sudoku.png to client/public/

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -20,23 +20,44 @@ export default function Portfolio(props) {
 
                     <div className='row'>
                         <div className='projectCon'>
-                                <div className='row'><span className='projectHeading'>ECHO</span></div>
-                                <div className='row'><span className='projectBody'>Social media application.</span></div>
-                                <div className='row'>
-                                    <Link target="_blank" to={'https://echo-application-d49a62c92c1a.herokuapp.com/'} className='projectBody'>Live version</Link>
-                                    <div className='empty'></div>
-                                    <Link target="_blank" to={'https://github.com/maximshilo/echo.git'} className='projectBody'>GitHub repository</Link>
-                                </div>
+                            <div className='row'><span className='projectHeading'>ECHO</span></div>
+                            <div className='row'><span className='projectBody'>Social media application.</span></div>
+                            <div className='row'>
+                                <Link target="_blank" to={'https://echo-application-d49a62c92c1a.herokuapp.com/'} className='projectBody'>Live version</Link>
+                                <div className='empty'></div>
+                                <Link target="_blank" to={'https://github.com/maximshilo/echo.git'} className='projectBody'>GitHub repository</Link>
+                            </div>
+                            <Link target="_blank" to={'https://echo-application-d49a62c92c1a.herokuapp.com/'}>
                                 <img className='projectImg' src='images/echo.png'></img>
-                                <div className='row'>
-                                    <p>
-                                    Echo is a Twitter-inspired application developed using React, MongoDB, Express, and Node.js (MERN stack). 
-                                    With Echo, users can post short messages, follow other users, and engage with content in real-time. 
-                                    Echo provides a seamless and intuitive user experience for sharing thoughts and connecting with others. 
-                                    Whether it's expressing opinions, sharing updates, or fostering discussions, 
+                            </Link>
+                            <div className='row'>
+                                <p>
+                                    Echo is a Twitter-inspired application developed using React, MongoDB, Express, and Node.js (MERN stack).
+                                    With Echo, users can post short messages, follow other users, and engage with content in real-time.
+                                    Echo provides a seamless and intuitive user experience for sharing thoughts and connecting with others.
+                                    Whether it's expressing opinions, sharing updates, or fostering discussions,
                                     Echo offers a familiar platform for social interaction and networking.
-                                    </p>
-                                </div>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className='projectCon'>
+                            <div className='row'><span className='projectHeading'>SUDOKU</span></div>
+                            <div className='row'><span className='projectBody'>A puzzle game.</span></div>
+                            <div className='row'>
+                                <Link target="_blank" to={'https://lively-shortbread-fdf4cb.netlify.app/'} className='projectBody'>Live version</Link>
+                                <div className='empty'></div>
+                                <Link target="_blank" to={'https://github.com/maximshilo/sudoku.git'} className='projectBody'>GitHub repository</Link>
+                            </div>
+                            <Link target="_blank" to={'https://lively-shortbread-fdf4cb.netlify.app/'}>
+                                <img className='projectImg' src='images/sudoku.png'></img>
+                            </Link>
+                            <div className='row'>
+                                <p>
+                                    A simple sudoku game developed using the React.js toolkit.
+                                    Deployed using Netlify's services.
+                                </p>
+                            </div>
                         </div>
 
                         <div className='projectCon'>
@@ -54,12 +75,6 @@ export default function Portfolio(props) {
                         <div className='projectCon'>
                             <div className='row'><span className='projectHeading'>DONIT</span></div>
                             <div className='row'><span className='projectBody'>Project management tool.</span></div>
-                            <img className='projectImg' src='images/construction.png'></img>
-                        </div>
-
-                        <div className='projectCon'>
-                            <div className='row'><span className='projectHeading'>SUDOKU</span></div>
-                            <div className='row'><span className='projectBody'>A puzzle game.</span></div>
                             <img className='projectImg' src='images/construction.png'></img>
                         </div>
 
