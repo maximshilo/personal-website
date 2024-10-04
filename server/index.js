e4cb629ac77e3b634e4deffd73f39b0e1b4f10a8
Maxim Shilo <maximshilo.dev@gmail.com>
2024-10-04 15:14:32 +0300

    test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -10,6 +10,8 @@ const app = express()
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use(cors())
 app.use(bodyParser.json())
+
+')
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
